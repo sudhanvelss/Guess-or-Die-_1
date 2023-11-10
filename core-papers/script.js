39576d29c825969b3d30a46848a7bf858251357b
sudhanvelss <sudhanssudhan83@gmail.com>
2023-11-10 08:46:29 +0530

    !!not so......

--- core-papers/script.js
+++ core-papers/script.js
@@ -2,85 +2,86 @@ const Keyboard_div = document.querySelector(".Keyboard")
 const merchant_body = document.querySelector(".merchant-box img")
 const Guess_text = document.querySelector(".Guess-text b")
 const word_display = document.querySelector(".word-display")
-let current_word, correctLetters = [], wrong_Guess_Count = 0;
+let current_word, correctLetters = "", wrong_Guess_Count = 0;
 const max_guesses = 6;
+let audio1 = new Audio("")
 var RandomWords = [
-    { 
-        word: "guitar",
-        hint: "A musical instrument with strings."
-    },
-    {
-        word: "oxygen",
-        hint: "A colorless, odorless gas essential for life."
-    },
-    {
-        word: "mountain",
-        hint: "A large natural elevation of the Earth's surface."
-    },
-    {
-        word: "painting",
-        hint: "An art form using colors on a surface to create images or expression."
-    },
+    // { 
+    //     word: "guitar",
+    //     hint: "A musical instrument with strings."
+    // },
+    // {
+    //     word: "oxygen",
+    //     hint: "A colorless, odorless gas essential for life."
+    // },
+    // {
+    //     word: "mountain",
+    //     hint: "A large natural elevation of the Earth's surface."
+    // },
+    // {
+    //     word: "painting",
+    //     hint: "An art form using colors on a surface to create images or expression."
+    // },
     {
         word: "astronomy",
         hint: "The scientific study of celestial objects and phenomena."
     },
-    {
-        word: "football",
-        hint: "A popular sport played with a spherical ball."
-    },
-    {
-        word: "chocolate",
-        hint: "A sweet treat made from cocoa beans."
-    },
-    {
-        word: "butterfly",
-        hint: "An insect with colorful wings and a slender body."
-    },
-    {
-        word: "history",
-        hint: "The study of past events and human civilization."
-    },
-    {
-        word: "pizza",
-        hint: "A savory dish consisting of a round, flattened base with toppings."
-    },
+    // {
+    //     word: "football",
+    //     hint: "A popular sport played with a spherical ball."
+    // },
+    // {
+    //     word: "chocolate",
+    //     hint: "A sweet treat made from cocoa beans."
+    // },
+    // {
+    //     word: "butterfly",
+    //     hint: "An insect with colorful wings and a slender body."
+    // },
+    // {
+    //     word: "history",
+    //     hint: "The study of past events and human civilization."
+    // },
+    // {
+    //     word: "pizza",
+    //     hint: "A savory dish consisting of a round, flattened base with toppings."
+    // },
     {
         word: "jazz",
         hint: "A genre of music characterized by improvisation and syncopation."
     },
-    {
-        word: "camera",
-        hint: "A device used to capture and record images or videos."
-    },
+    // {
+    //     word: "camera",
+    //     hint: "A device used to capture and record images or videos."
+    // },
     {
         word: "diamond",
         hint: "A precious gemstone known for its brilliance and hardness."
     },
-    {
-        word: "adventure",
-        hint: "An exciting or daring experience."
-    },
+    // {
+    //     word: "adventure",
+    //     hint: "An exciting or daring experience."
+    // },
     {
         word: "science",
         hint: "The systematic study of the structure and behavior of the physical and natural world."
     },
-    {
-        word: "bicycle",
-        hint: "A human-powered vehicle with two wheels."
-    },
-    {
-        word: "sunset",
-        hint: "The daily disappearance of the sun below the horizon."
-    },
-    {
-        word: "coffee",
-        hint: "A popular caffeinated beverage made from roasted coffee beans."
-    },
-    {
-        word: "dance",
-        hint: "A rhythmic movement of the body often performed to music."
-    },
+    // {
+    //     word: "bicycle",
+    //     hint: "A human-powered vehicle with two wheels."
+    // },
+    // {
+    //     word: "sunset",
+    //     hint: "The daily disappearance of the sun below the horizon."
+    // },
+    // {
+    //     word: "coffee",
+    //     hint: "A popular caffeinated beverage made from roasted coffee beans."
+    // },
+    // {
+    //     word: "dance",
+    //     hint: "A rhythmic movement of the body often performed to music."
+    // },
     {
         word: "galaxy",
         hint: "A vast system of stars, gas, and dust held together by gravity."
@@ -89,10 +90,10 @@ var RandomWords = [
         word: "orchestra",
         hint: "A large ensemble of musicians playing various instruments."
     },
-    {
-        word: "volcano",
-        hint: "A mountain or hill with a vent through which lava, rock fragments, hot vapor, and gas are ejected."
-    },
+    // {
+    //     word: "volcano",
+    //     hint: "A mountain or hill with a vent through which lava, rock fragments, hot vapor, and gas are ejected."
+    // },
     {
         word: "novel",
         hint: "A long work of fiction, typically with a complex plot and characters."
@@ -109,22 +110,22 @@ var RandomWords = [
         word: "architecture",
         hint: "The art and science of designing and constructing buildings."
     },
-    {
-        word: "ballet",
-        hint: "A classical dance form characterized by precise and graceful movements."
-    },
-    {
-        word: "astronaut",
-        hint: "A person trained to travel and work in space."
-    },
-    {
-        word: "waterfall",
-        hint: "A cascade of water falling from a height."
-    },
-    {
-        word: "technology",
-        hint: "The application of scientific knowledge for practical purposes."
-    },
+    // {
+    //     word: "ballet",
+    //     hint: "A classical dance form characterized by precise and graceful movements."
+    // },
+    // {
+    //     word: "astronaut",
+    //     hint: "A person trained to travel and work in space."
+    // },
+    // {
+    //     word: "waterfall",
+    //     hint: "A cascade of water falling from a height."
+    // },
+    // {
+    //     word: "technology",
+    //     hint: "The application of scientific knowledge for practical purposes."
+    // },
     {
         word: "rainbow",
         hint: "A meteorological phenomenon that is caused by reflection, refraction, and dispersion of light."
@@ -266,11 +267,12 @@ var RandomWords = [
         hint: "Exceptionally clever, talented, or impressive."
     },
 ];
-
+console.log("ch", correctLetters )
 
 var Random_words = () => {
-    // to select the random words in the given list we making this fucntion.
+    // to select the random words in the given list we making this function.
     let { word, hint } =  RandomWords[Math.floor(Math.random() * RandomWords.length)];
+    localStorage.setItem('word', word)
     current_word = word;
     console.log( word);
     document.querySelector(".hint-text b").innerHTML = hint;
@@ -281,7 +283,7 @@ const initGame = (button, letterclicked) => {
     if(current_word.includes(letterclicked)){
         [...current_word].forEach((letter, index) =>{
             if(letter === letterclicked){
-                correctLetters.push(letter);
+                correctLetters += letter;
                 word_display.querySelectorAll("li")[index].innerHTML = letter;
             }
         })
@@ -289,10 +291,11 @@ const initGame = (button, letterclicked) => {
         wrong_Guess_Count++;
         merchant_body.src = `./../images/hangman-${wrong_Guess_Count}.svg`;
     }
-    if (correctLetters.length == current_word && correctLetters < max_guesses){
-        console.log(window.location.reload);
-    }else if (correctLetters !== current_word && correctLetters.length >= max_guesses){
-        console.log(window.location.href = "gameover.html")
+    if (correctLetters.length === current_word.length){
+    
+        location.reload();
+    }else if (correctLetters !== current_word && wrong_Guess_Count >= max_guesses){
+        window.location.href = "gameover.html"
     }
 
     button.disabled = true;
